bd7c0cb0f451391b623d32f1e9faab5f4801dbfb
unknown <jshrey1995@gmail.com>
2023-08-07 23:23:56 +0530

    bat file added

---
+++ 2.0/run_FINAL.bat
@@ -0,0 +1,3 @@
+@echo off
+cmd /k "cd C:\Users\Shrey\OneDrive\Desktop\latest_tally_to_powerbi\2.0\tally\Scripts & activate & cd /d C:\Users\Shrey\OneDrive\Desktop\latest_tally_to_powerbi\2.0 & python main.py"
+pause
